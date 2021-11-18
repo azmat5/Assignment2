ab0f89afe7ce6c279a7341c3df2145f1a904d752
azmat5 <94361663+azmat5@users.noreply.github.com>
2021-11-18 22:35:16 +0500

    Add files via upload

---
+++ Azmat2.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+void main () {
+// First Program
+
+//  String V = "Dart Course";
+//  String V2 = (V[0].toUpperCase()+ V.substring(1));
+//  print(V2);
+//  print(V.toLowerCase());
+//  print(V.toUpperCase());
+
+// Second Program
+
+//  stdout.write("Enter The Number");
+//  int? A = int.parse(stdin.readLineSync()!);
+//  num Total = (A+5)*10/2;
+//  print(Total);
+
+// Third Program
+
+//  int u = 3;
+//  int v = 4;
+//  var Result = --u - --v + ++v + --v;
+//  print(Result);
+//  var v1 = --u;
+//  var v2 = --u - --v;
+//  var v3 = --u - --v + ++v;
+//  var v4 = --u - --v + ++v + v--;
+//  print(v1);
+//  print(v2);
+//  print(v3);
+//  print(v4);
+
+// Fourth Program
+
+//  String Au = "Kamran";
+//  String Av = "KAMRAN";
+//  print(Av.compareTo(Au));
+
+// Fifth Program
+
+//  stdout.write("User Input : ");
+//  String n = (stdin.readLineSync()!);
+//  print(n.length);
+
+// Sixth Program
+
+//  String M = "Ubaid";
+//  int G = M.codeUnitAt(0);
+//  print(G);
+
+// Seventh Program
+
+//  stdout.write("User Input : ");
+//  String L = (stdin.readLineSync()!);
+//  var Result2= L.substring(3,6);
+//  print("$Result2");
+
+// Eighth Program
+
+// String P = "Sir Zubair";
+// var P2 = P.split("");
+// print(P2);
+
+}
